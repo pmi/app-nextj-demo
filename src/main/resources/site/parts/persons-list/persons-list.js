6c95a717bac32dc3a02db92959bb8061b29e0947
Pavel Milkevich <pavel.milkevich@gmail.com>
2021-11-27 11:56:22 +0300

    A part or two #13

--- src/main/resources/site/parts/persons-list/persons-list.js
+++ src/main/resources/site/parts/persons-list/persons-list.js
@@ -2,10 +2,7 @@ var proxy = require('/lib/frontend-proxy/proxy');
 
 exports.get = function (req) {
 
-    log.info(`Default page controller:\nUrl: ${req.path}\nmode: ${req.mode}`);
+    log.info(`Persons list controller:\nUrl: ${req.path}\nmode: ${req.mode}`);
 
     return proxy.get(req);
 }
-
-
-
